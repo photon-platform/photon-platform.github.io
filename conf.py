f5e06435b644a1dd3aa91d6405d6708b8e4c57e8
phiarchitect <phi@phiarchitect.com>
2022-12-14 08:31:35 -0800

    cleanup

---
+++ conf.py
@@ -0,0 +1,33 @@
+from ablog.conf import *
+
+org = 'photon-platform'
+org_name = 'PHOTON platform'
+
+repo = ''
+repo_name = ''
+
+blog_title = f'{org_name}'
+project = f'{org_name}'
+version = ""  # The short X.Y version.
+release = ""  # The full version, including alpha/beta/rc tags.
+
+copyright = f'2022, {org_name}'
+author = "phi ARCHITECT"
+
+# Base URL for the website, required for generating feeds.
+# e.g. blog_baseurl = "http://example.com/"
+blog_baseurl = f'https://{org}.github.io'
+html_base_url = blog_baseurl
+html_baseurl = blog_baseurl
+
+blog_authors = {
+    "phi": ("phi ARCHITECT", None),
+}
+
+html_theme_options = {
+    'logo': 'logo.png',
+    'logo_name': True,
+    'github_user': org,
+    'github_button': True,
+}
+
